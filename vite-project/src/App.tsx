af681bad7844921439b2a884db1c108a5d04bb21
TLRKiliann <philogenie@protonmail.com>
2022-11-02 16:13:42 +0100

    add first component to test

--- vite-project/src/App.tsx
+++ vite-project/src/App.tsx
@@ -1,34 +1,35 @@
-import { useState } from 'react'
+//import { useState } from 'react'
+import SimpleBox from './components/SimpleBox'
 import reactLogo from './assets/react.svg'
 import './App.scss'
 
-function App() {
-  const [count, setCount] = useState(0)
+const App:React.FC = () => {
 
   return (
     <div className="App">
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <h1 className="text-3xl font-serif font-bold">
+          Wellcome !
+        </h1>
       </div>
-      <h1 className="text-3xl font-bold underline">
-        Hello world!
-      </h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+      <div className="please text-center
+        font-serif font-bold text-2xl p-3">
+        <span>
+          Ple
+        </span>
+        <span>
+          ase
+        </span>
+        <span>
+          &nbsp;log
+        </span>
+        <span>
+          in
+        </span>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+
+      <SimpleBox />
+
     </div>
   )
 }
